f2b966064bcf3bb37446bf59cb6a0acdca4044fa
liangkunyao <liangkunyao@hotmail.com>
2017-05-28 09:38:31 +0800

    initial commit

---
+++ iOSBaseDevProject/iOSBaseDevProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSBaseDevProject
+//
+//  Created by 梁坤尧 on 2017/5/28.
+//  Copyright © 2017年 梁坤尧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSBaseDevProject/iOSBaseDevProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSBaseDevProject
+//
+//  Created by 梁坤尧 on 2017/5/28.
+//  Copyright © 2017年 梁坤尧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
