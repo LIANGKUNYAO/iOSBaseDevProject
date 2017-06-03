a52d4c0377fc79897ca86d7fbaed0ca4983b65ab
liangkunyao <liangkunyao@hotmail.com>
2017-06-03 19:32:36 +0800

    del some macros

--- iOSBaseDevProject/iOSBaseDevProject/Macros.h
+++ iOSBaseDevProject/iOSBaseDevProject/Macros.h
@@ -1,29 +1,3 @@
-#import <mach/mach_time.h>  // for mach_absolute_time() and friends
-                            // adapted from http://blog.bignerdranch.com/316-a-timing-utility/
-
-#import <CoreGraphics/CGBase.h>
-
-// Adapted from Will Shipley http://blog.wilshipley.com/2005/10/pimp-my-code-interlude-free-code.html
-static inline BOOL IsEmpty(id thing) {
-    return thing == nil || [thing isEqual:[NSNull null]]
-        || ([thing respondsToSelector:@selector(length)]
-        && [(NSData *)thing length] == 0)
-        || ([thing respondsToSelector:@selector(count)]
-        && [(NSArray *)thing count] == 0);
-}
-
-static inline NSString *StringFromObject(id object) {
-	if (object == nil || [object isEqual:[NSNull null]]) {
-		return @"";
-	} else if ([object isKindOfClass:[NSString class]]) {
-		return object;
-	} else if ([object respondsToSelector:@selector(stringValue)]){
-		return [object stringValue];
-	} else {
-		return [object description];
-	}
-}
-
 #pragma mark -
 #pragma mark iOS Version
 
@@ -34,12 +8,6 @@ static inline NSString *StringFromObject(id object) {
 #define IOS_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
 
 #pragma mark -
-#pragma mark Syntactic sugar
-
-#define NOT_ANIMATED NO
-#define ANIMATED YES
-
-#pragma mark -
 #pragma mark UIColor
 
 // example usage: UIColorFromHex(0x9daa76)
@@ -47,36 +15,6 @@ static inline NSString *StringFromObject(id object) {
 #define UIColorFromHex(hexValue)            UIColorFromHexWithAlpha(hexValue,1.0)
 #define UIColorFromRGBA(r,g,b,a)            [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 #define UIColorFromRGB(r,g,b)               UIColorFromRGBA(r,g,b,1.0)
-
-#pragma mark -
-#pragma mark Collections
-
-#define IDARRAY(...) (id []){ __VA_ARGS__ }
-#define IDCOUNT(...) (sizeof(IDARRAY(__VA_ARGS__)) / sizeof(id))
-
-#define ARRAY(...) [NSArray arrayWithObjects: IDARRAY(__VA_ARGS__) count: IDCOUNT(__VA_ARGS__)]
-
-#define DICT(...) DictionaryWithIDArray(IDARRAY(__VA_ARGS__), IDCOUNT(__VA_ARGS__) / 2)
-
-//The helper function unpacks the object array and then calls through to NSDictionary to create the dictionary:
-static inline NSDictionary *DictionaryWithIDArray(id *array, NSUInteger count) {
-    id keys[count];
-    id objs[count];
-    
-    for(NSUInteger i = 0; i < count; i++) {
-        keys[i] = array[i * 2];
-        objs[i] = array[i * 2 + 1];
-    }
-    
-    return [NSDictionary dictionaryWithObjects: objs forKeys: keys count: count];
-}
-#define POINTERIZE(x) ((__typeof__(x) []){ x })
-#define NSVALUE(x) [NSValue valueWithBytes: POINTERIZE(x) objCType: @encode(__typeof__(x))]
-
-#pragma mark -
-#pragma mark Blocks
-
-#define BLOCK_SAFE_RUN(block, ...) block ? block(__VA_ARGS__) : nil
 
 #pragma mark -
 #pragma mark Logging
@@ -151,19 +89,6 @@ static inline NSDictionary *DictionaryWithIDArray(id *array, NSUInteger count) {
 
 #define DEGREES_TO_RADIANS(degrees) degrees * M_PI / 180
 
-static inline void TimeThisBlock (void (^block)(void), NSString *message) {
-    mach_timebase_info_data_t info;
-    if (mach_timebase_info(&info) != KERN_SUCCESS) {
-        block();
-        return;
-    };
-    
-    uint64_t start = mach_absolute_time ();
-    block ();
-    uint64_t end = mach_absolute_time ();
-    uint64_t elapsed = end - start;
-    
-    uint64_t nanos = elapsed * info.numer / info.denom;
-    LOG(@"Took %f seconds to %@", (CGFloat)nanos / NSEC_PER_SEC, message);
-}
+#define StartTag CFAbsoluteTime start = CFAbsoluteTimeGetCurrent();
+#define EndTag  NSLog(@"Time: %f", CFAbsoluteTimeGetCurrent() - start)
 
